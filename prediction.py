eb9000380434128e293f1195d73b310e6382f2a2
chava manish <52528480+Chavamanish@users.noreply.github.com>
2021-10-26 00:07:18 +0530

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,7 @@
+import adaboost_classifier
+import os
+x=5
+def get_values(transmission,authorization,oversubscription,net_design,hack_prone,protocol,comm_prot_label,filesize_trans,firewalls,hard_comp,router,devices):
+    print("successful")
+    print(x)
+    adaboost_classifier.classifier.predict(netId,hard_comp,router,devices,)
